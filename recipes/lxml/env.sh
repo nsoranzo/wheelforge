26e0232f5ef0c981bacf5dafd4237ef7961af16c
Nicola Soranzo <nicola.soranzo@gmail.com>
2026-08-17 01:16:27 +0200

    Remove CIBW_PRERELEASE_PYTHONS from env.sh
    Removed deprecated CIBW_PRERELEASE_PYTHONS environment variable. It's also unnecessary as Python 3.15 is at release-candidate stage, not beta.

--- recipes/lxml/env.sh
+++ recipes/lxml/env.sh
@@ -1,4 +1,3 @@
 export CIBW_BUILD='cp315-* cp315t-*'
 # Workaround for https://github.com/lxml/lxml/pull/526
 export CIBW_BEFORE_ALL='curl -sSfLO https://raw.githubusercontent.com/lxml/lxml/refs/tags/lxml-6.1.1/patch_lxml_deplibs.py && curl -sSfLO https://raw.githubusercontent.com/lxml/lxml/refs/tags/lxml-6.1.1/libxslt-1.1.43-backport1.patch'
-export CIBW_PRERELEASE_PYTHONS=True  # Can be removed once Python 3.15 is officially released
